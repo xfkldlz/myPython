406189a6d7a2b6e23e995ca4007299c91cef46ba
xfkldlz <75309625+xfkldlz@users.noreply.github.com>
2022-08-05 16:56:06 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env pythons
 # -*- coding: utf-8 -*-
-
-def g(i, a):
-    for j in range(len(a)):
-        if not(i % a[j]):
-            return -1
-    return i
-
+def f(a):
+    def g(i):
+        for j in range(len(a)):
+            if not (i % a[j]):
+                return -1
+            elif a[j]*a[j] > i:
+                return i
+        return i
+    return g
 
 a = [3, 5, 7]
-b = [i for i in range(a[len(a) - 1] + 2, 100, 2) if g(i, a) != -1]
+h = f(a)
+b = [i for i in range(a[len(a) - 1] + 2, 100, 2) if h(i) != -1]
 a += b
 a.insert(0,2)
 print(a)
